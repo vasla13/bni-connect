c984d35493a36ba9d7bfed1e6ee5f905ff16a107
Vasla13 <eripet05@hotmail.com>
2026-01-17 12:22:35 +0100

    fix security

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,16 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// COLLE TA CONFIGURATION ICI (Celle que tu viens de copier)
+// Configuration sécurisée via variables d'environnement
 const firebaseConfig = {
-  apiKey: "AIzaSyDHGh_4GC2TOw-a5rlBWRkx93rBLrdnjJY",
-  authDomain: "bni-connect-40840.firebaseapp.com",
-  projectId: "bni-connect-40840",
-  storageBucket: "bni-connect-40840.firebasestorage.app",
-  messagingSenderId: "1023058885069",
-  appId: "1:1023058885069:web:d637f9e54c4cc4482cac84",
-  measurementId: "G-LD3XJ9M249"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
